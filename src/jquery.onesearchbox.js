576b420d886b41558a1691b3a609b043f4eeb76f
Pablo Novas <pjnovas@gmail.com>
2012-07-16 15:51:25 -0300

    fixed clear for categories combo

--- src/jquery.onesearchbox.js
+++ src/jquery.onesearchbox.js
@@ -244,7 +244,7 @@
 					$('li.' + catsToClear[j], $(data.options.tags)).remove();
 				else $('li', $(data.options.tags)).remove(); 
 			}
-			
+				
 			if(!catsToClear.length){
 				removeSelection();
 			}
@@ -253,6 +253,19 @@
 					removeSelection(catsToClear[j]);
 				}
 			}
+
+			//move selection of combo to All
+			var $select = $('.ui-combobox').prev();
+			$select.change(function() {
+        var $this = $(this),
+            $input = $this.next().children('input'),
+            $el = $this.find('option:selected');
+            
+        $input.val($this.children(':selected').text());
+        $input.trigger('autocompleteselect', {item: {label: $el.text(), option: $el[0], value: $el.val()}} );
+			});
+			
+			$select.val('Todos').trigger('change');
 			
 			return this;
     },
